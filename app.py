a4ba9a5fe320ef8a3c4f063e0041bf35bb23b2fd
oliviagrima <100474858@alumnos.uc3m.es>
2024-10-08 17:26:35 +0200

    self

--- app.py
+++ app.py
@@ -1,37 +1,56 @@
 
 class Aplication:
-    def inicio():
+    def inicio(self):
         print("\n---------------------------Iniciando la aplicación---------------------------")
         print("\nQue desea hacer?")
         print("\n\t1. Registrarse")
         print("\n\t2. Iniciar sesión")
         print("\n\t3. Salir")
-    
-        comando = input("\nIngrese el número de la acción que desea realizar: ")
 
-        if comando == "1":
-            Aplication.registro()
-        elif comando == "2":
-            Aplication.iniciar_sesion()
-        elif comando == "3":
-            print("\n--------------------------Saliendo de la aplicación--------------------------")
-        else:
-            print("\nComando no válido")
-            Aplication.inicio()
-    
-    def registro():
+        seguir_en_app = True
+        while seguir_en_app:
+            try:
+                comando = input("\nIngrese el número de la acción que desea realizar: ")
+
+                if comando == "1":
+                    self.registro() 
+                elif comando == "2":
+                    self.iniciar_sesion()
+                elif comando == "3":
+                    print("\n---------------------------Saliendo de la aplicación---------------------------")
+                    seguir_en_app = False
+                else:
+                    comando = input("\nComando no válido, por favor, ingrese un número del 1 al 3: ")
+                    if comando == "1":
+                        self.registro()
+                    elif comando == "2":
+                        self.iniciar_sesion()
+                    elif comando == "3":
+                        print("\n---------------------------Saliendo de la aplicación---------------------------")
+                        seguir_en_app = False
+
+            except KeyboardInterrupt:
+                print("\n---------------------------Saliendo de la aplicación---------------------------")
+                seguir_en_app = False
+
+    def registro(self):
         print("\n------------------------------Registro------------------------------")
         usuario = input("\nIngrese el nombre de usuario: ")
         contraseña = input("\nIngrese la contraseña: ")
         confirmar_contraseña = input("\nConfirme la contraseña: ")
 
+        if usuario == "" or contraseña == "" or confirmar_contraseña == "":
+            print("\nPor favor, complete todos los campos")
+
+        #if usuario == un usuario ya registrado --> error
+
         while contraseña != confirmar_contraseña:
-            print("\nLas contraseñas no coinciden")
+            print("\nLas contraseñas no coinciden, por favor, vuelva a intentarlo")
             contraseña = input("\nIngrese la contraseña: ")
             confirmar_contraseña = input("\nConfirme la contraseña: ")
             
         print("\nUsuario registrado con éxito")
 
-    def iniciar_sesion():
+    def iniciar_sesion(self):
         print("\n---------------------------Inicio de sesión---------------------------")
 
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from app import Aplication
 
 class Main:
-    Aplication.inicio()
+    def __init__(self):
+        self.app = Aplication()
+
+    def run(self):
+        self.app.inicio()
+    
+if __name__ == "__main__":
+    main = Main()
+    main.run()
