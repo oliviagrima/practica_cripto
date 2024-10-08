aa4685e7fbfe833e6770528905443b602eb44a1b
inigo <100461095@alumnos.uc3m.es>
2024-10-08 15:59:00 +0200

    cosas

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
 
 class Aplication():
-    def __init__(self):
-        ...
-        
+    def inicio():
+        print("\n---------------------------Iniciando la aplicación---------------------------")
+        print("\nQue desea hacer?")
+        print("\n\t1. Registrarse")
+        print("\n\t2. Iniciar sesión")
+        print("\n\t3. Salir")
+    
+        comando = input("\nIngrese el número de la acción que desea realizar: ")
+
+        if comando == "1":
+            Aplication.registro()
+        elif comando == "2":
+            Aplication.iniciar_sesion()
+        elif comando == "3":
+            print("\n---------------------------Saliendo de la aplicación---------------------------")
+        else:
+            print("\nComando no válido")
+            Aplication.inicio()
+    
+    def registro():
+        print("\n---------------------------Registro---------------------------")
+    def iniciar_sesion():
+        print("\n---------------------------Iniciar sesión---------------------------")
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from app import Aplication
+
+class Main ():
+    def __init__():
+        Aplication.inicio()
